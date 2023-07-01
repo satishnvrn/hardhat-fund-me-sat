5015400bffd16acbf0ba017b43bcf79355520f94
satishnvrn <nvrn.satish@gmail.com>
2023-07-01 16:22:19 +0530

    Formats code

--- contracts/FundMe.sol
+++ contracts/FundMe.sol
@@ -1,78 +1,84 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.18;
 
-import "@chainlink/contracts/src/v0.8/interfaces/AggregatorV3Interface.sol";
-import "./PriceConverter.sol";
+import '@chainlink/contracts/src/v0.8/interfaces/AggregatorV3Interface.sol';
+import './PriceConverter.sol';
 
 error NotOwner();
 
 /** @title A contract for crowd funding
-*   @author Satish NVRN
-*   @notice This contract is to demo a sample funding contract
-*   @dev This implements price feeds as our library
-*/
+ *   @author Satish NVRN
+ *   @notice This contract is to demo a sample funding contract
+ *   @dev This implements price feeds as our library
+ */
 contract FundMe {
-    using PriceConverter for uint256;
+  using PriceConverter for uint256;
 
-    uint256 public constant MINIMUM_USD = 50;
+  uint256 public constant MINIMUM_USD = 50;
 
-    address[] private funders;
-    mapping(address => uint256) private addressToAmountFunded;
+  address[] private funders;
+  mapping(address => uint256) private addressToAmountFunded;
 
-    address private immutable i_owner;
+  address private immutable i_owner;
 
-    AggregatorV3Interface private priceFeed;
+  AggregatorV3Interface private priceFeed;
 
-    constructor(address priceFeedAddress) {
-        i_owner = msg.sender;
-        priceFeed = AggregatorV3Interface(priceFeedAddress);
+  constructor(address priceFeedAddress) {
+    i_owner = msg.sender;
+    priceFeed = AggregatorV3Interface(priceFeedAddress);
+  }
+
+  modifier onlyOwner() {
+    // require(msg.sender == i_owner);
+    if (msg.sender != i_owner) {
+      revert NotOwner();
+    }
+    _;
+  }
+
+  function fund() public payable {
+    require(
+      msg.value.getConversionRate(priceFeed) >= MINIMUM_USD,
+      "Didn't send enough ETH"
+    );
+    funders.push(msg.sender);
+    addressToAmountFunded[msg.sender] += msg.value;
+    // 20000000000000000
+  }
+
+  function getRateForETH(uint256 ethAmount) public view returns (uint256) {
+    uint256 answer = ethAmount.getConversionRate(priceFeed);
+    return answer;
+  }
+
+  function withdraw() public onlyOwner {
+    for (uint256 funderIndex = 0; funderIndex < funders.length; funderIndex++) {
+      address funder = funders[funderIndex];
+      addressToAmountFunded[funder] = 0;
     }
 
-    modifier onlyOwner {
-        // require(msg.sender == i_owner);
-        if (msg.sender != i_owner) {
-            revert NotOwner();
-        }
-        _;
-    }
+    funders = new address[](0);
+    (bool callSuccess, ) = payable(msg.sender).call{
+      value: address(this).balance
+    }('');
+    require(callSuccess, 'Call failed');
+  }
 
-    function fund() public payable {
-        require(msg.value.getConversionRate(priceFeed) >= MINIMUM_USD, "Didn't send enough ETH");
-        funders.push(msg.sender);
-        addressToAmountFunded[msg.sender] += msg.value;
-        // 20000000000000000
-    }
+  receive() external payable {
+    fund();
+  }
 
-    function getRateForETH(uint256 ethAmount) public view returns(uint256) {
-        uint256 answer = ethAmount.getConversionRate(priceFeed);
-        return answer;
-    }
+  function getPriceFeed() public view returns (AggregatorV3Interface) {
+    return priceFeed;
+  }
 
-    function withdraw() public onlyOwner {
-        for (uint256 funderIndex = 0; funderIndex < funders.length; funderIndex++) 
-        {
-            address funder = funders[funderIndex];
-            addressToAmountFunded[funder] = 0;
-        }
+  function getFunders(uint256 index) public view returns (address) {
+    return funders[index];
+  }
 
-        funders = new address[](0);
-        (bool callSuccess, ) = payable(msg.sender).call{value: address(this).balance}("");
-        require(callSuccess, "Call failed");
-    }
-
-    receive() external payable {
-        fund();
-    }
-
-    function getPriceFeed() public view returns (AggregatorV3Interface) {
-        return priceFeed;
-    }
-
-    function getFunders(uint256 index) public view returns (address) {
-        return funders[index];
-    }
-
-    function getAddressToAmountFunded(address funderAddress) public view returns (uint256) {
-        return addressToAmountFunded[funderAddress];
-    }
+  function getAddressToAmountFunded(
+    address funderAddress
+  ) public view returns (uint256) {
+    return addressToAmountFunded[funderAddress];
+  }
 }
--- test/unit/FundMe.test.ts
+++ test/unit/FundMe.test.ts
@@ -8,36 +8,44 @@ describe('FundMe', async () => {
   let fundMe: FundMe;
   let deployer: Address;
   let mockV3Aggregator: MockV3Aggregator;
-  const sendValue = ethers.parseEther("0.03");
+  const sendValue = ethers.parseEther('0.03');
   beforeEach(async () => {
     if (!developmentChains.includes(network.name)) {
-      throw "You need to be on a development chain to run tests"
+      throw 'You need to be on a development chain to run tests';
     }
     deployer = (await getNamedAccounts()).deployer;
 
     console.log('deploying all contracts');
     const deploymentResults = await deployments.fixture(['all']);
 
-    const fundMeAddress: string = deploymentResults["FundMe"]?.address;
-    fundMe = await ethers.getContractAt("FundMe", fundMeAddress) as unknown as FundMe;
-    const mockV3AggregatorAddress: string = deploymentResults["MockV3Aggregator"]?.address;
-    mockV3Aggregator = await ethers.getContractAt("MockV3Aggregator", mockV3AggregatorAddress) as unknown as MockV3Aggregator;
+    const fundMeAddress: string = deploymentResults['FundMe']?.address;
+    fundMe = (await ethers.getContractAt(
+      'FundMe',
+      fundMeAddress
+    )) as unknown as FundMe;
+    const mockV3AggregatorAddress: string =
+      deploymentResults['MockV3Aggregator']?.address;
+    mockV3Aggregator = (await ethers.getContractAt(
+      'MockV3Aggregator',
+      mockV3AggregatorAddress
+    )) as unknown as MockV3Aggregator;
   });
 
-  describe("Constructor", async () => {
-    it("sets the aggregator addresses correctly",async () => {
+  describe('Constructor', async () => {
+    it('sets the aggregator addresses correctly', async () => {
       const response: string = await fundMe.getPriceFeed();
-      const mockV3AggregatorAddress: string = await mockV3Aggregator.getAddress();
+      const mockV3AggregatorAddress: string =
+        await mockV3Aggregator.getAddress();
       assert.equal(response, mockV3AggregatorAddress);
     });
   });
 
-  describe("fund", async () => {
+  describe('fund', async () => {
     it("Fails if you don't send enough ETH", async () => {
       await expect(fundMe.fund()).to.be.revertedWith("Didn't send enough ETH");
     });
 
-    it("fund the contract and check funders and addressToAmountFunded map", async () => {
+    it('fund the contract and check funders and addressToAmountFunded map', async () => {
       await fundMe.fund({ value: sendValue });
       const latestFunder = await fundMe.getFunders(0);
       assert.equal(latestFunder, deployer);
@@ -47,27 +55,37 @@ describe('FundMe', async () => {
     });
   });
 
-  describe("withdraw", async () => {
+  describe('withdraw', async () => {
     beforeEach(async () => {
       await fundMe.fund({ value: sendValue });
     });
-    
-    it("withdraw by deployer", async () => {
+
+    it('withdraw by deployer', async () => {
       const fundMeAddress = await fundMe.getAddress();
       const provider = fundMe.runner?.provider;
 
-      const startingFundMeBalance = await provider?.getBalance(fundMeAddress) || BigInt(0);
-      const startingDeployerBalance = await provider?.getBalance(deployer) || BigInt(0);
+      const startingFundMeBalance =
+        (await provider?.getBalance(fundMeAddress)) || BigInt(0);
+      const startingDeployerBalance =
+        (await provider?.getBalance(deployer)) || BigInt(0);
 
       const transactionResponse = await fundMe.withdraw();
-      const transactionReceipt = await transactionResponse.wait() || {gasUsed: BigInt(0), gasPrice: BigInt(0)};
+      const transactionReceipt = (await transactionResponse.wait()) || {
+        gasUsed: BigInt(0),
+        gasPrice: BigInt(0),
+      };
       const { gasUsed, gasPrice } = transactionReceipt;
       const gasCost = gasUsed * gasPrice;
 
-      const endingFundMeBalance = await provider?.getBalance(fundMeAddress) || BigInt(0);
-      const endingDeployerBalance = await provider?.getBalance(deployer) || BigInt(0);
+      const endingFundMeBalance =
+        (await provider?.getBalance(fundMeAddress)) || BigInt(0);
+      const endingDeployerBalance =
+        (await provider?.getBalance(deployer)) || BigInt(0);
       assert.equal(endingFundMeBalance, BigInt(0));
-      assert.equal((startingFundMeBalance + startingDeployerBalance).toString(), (endingDeployerBalance + gasCost).toString());
+      assert.equal(
+        (startingFundMeBalance + startingDeployerBalance).toString(),
+        (endingDeployerBalance + gasCost).toString()
+      );
     });
   });
 });
